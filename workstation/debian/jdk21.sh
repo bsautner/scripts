6f3416a63e9ebc6c58c457e28d0b7235ad280336
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2023-12-08 09:30:58 -0500

    Create jdk21.sh

---
+++ workstation/debian/jdk21.sh
@@ -0,0 +1,16 @@
+cd ~
+wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
+sudo mkdir /usr/lib/jvm
+sudo mv ./jdk-21_linux-x64_bin.tar.gz /usr/lib/jvm
+cd /usr/lib/jvm
+sudo tar -xvzf jdk-21_linux-x64_bin.tar.gz
+sudo rm jdk-21_linux-x64_bin.tar.gz
+sudo mv jdk-21.0.1 jdk-21
+cd ~
+bash -c 'echo "PATH=/usr/lib/jvm/jdk-21/bin:$PATH" >> ~/.profile'
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk-21/bin/java" 0
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk-21/bin/javac" 0
+sudo update-alternatives --set java /usr/lib/jvm/jdk-21/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/jdk-21/bin/javac
+
+ 
