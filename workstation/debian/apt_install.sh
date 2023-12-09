e96b6fbb180a80c5458b098fceb95b251098d340
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2023-12-09 09:04:02 -0500

    Update apt_install.sh

--- workstation/debian/apt_install.sh
+++ workstation/debian/apt_install.sh
@@ -3,7 +3,7 @@ echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org
 wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
 
 
-sudo apt update --fixe-missing && sudo apt upgrade
+sudo apt update --fixe-missing && sudo apt upgrade -y && sudo apt dist-upgrade -y
 sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install wine32:i386
 sudo apt install google-chrome-stable chrome-gnome-shell gnome-browser-connector apt-transport-https curl -y
 sudo apt install nvidia-xconfig cpu-checker qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
