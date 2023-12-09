6dbc2e6d850aade536b95beb3ae4477cee6da2a8
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2023-12-09 09:01:39 -0500

    Update apt_install.sh

--- workstation/debian/apt_install.sh
+++ workstation/debian/apt_install.sh
@@ -1,1 +1,11 @@
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
 
+
+sudo apt update --fixe-missing && sudo apt upgrade
+sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install wine32:i386
+sudo apt install google-chrome-stable chrome-gnome-shell gnome-browser-connector apt-transport-https curl -y
+sudo apt install nvidia-xconfig cpu-checker qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
+sudo apt install lutris
+sudo apt install nvidia-driver-libs:i386
