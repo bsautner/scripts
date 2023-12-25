b11f697559956811ecc4d0f95848e2873b373a7c
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2023-12-25 10:00:33 -0500

    Create chrome.sh

---
+++ workstation/debian/chrome.sh
@@ -0,0 +1,5 @@
+sudo apt install apt-transport-https curl -y
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' 
+sudo apt update
+sudo apt install google-chrome-stable -y
