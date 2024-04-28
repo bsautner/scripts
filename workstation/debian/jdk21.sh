3c5ce2e4bccc2d4d64ca4bd5fe62fa19a3c8dbf4
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2024-04-28 11:03:16 -0400

    Update jdk21.sh

--- workstation/debian/jdk21.sh
+++ workstation/debian/jdk21.sh
@@ -5,7 +5,7 @@ sudo mv ./jdk-21_linux-x64_bin.tar.gz /usr/lib/jvm
 cd /usr/lib/jvm
 sudo tar -xvzf jdk-21_linux-x64_bin.tar.gz
 sudo rm jdk-21_linux-x64_bin.tar.gz
-sudo mv jdk-21.0.1 jdk-21
+sudo mv jdk-21.* jdk-21
 cd ~
 bash -c 'echo "PATH=/usr/lib/jvm/jdk-21/bin:$PATH" >> ~/.profile'
 sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk-21/bin/java" 0
